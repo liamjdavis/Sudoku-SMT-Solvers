16457f8df681afbbfe8ee3d474e6be10dd4bb5eb
liamjdavis <davisliam123@gmail.com>
2024-11-26 16:02:25 -0800

    removed DPLL(T) from init

--- sudoku_smt_solvers/__init__.py
+++ sudoku_smt_solvers/__init__.py
@@ -1,3 +1,3 @@
 from .benchmarks import BenchmarkRunner, SudokuGenerator
-from .solvers import CVC5Solver, DPLLSolver, DPLLTSolver, Z3Solver
+from .solvers import CVC5Solver, DPLLSolver, Z3Solver
 from .utils import Parser, Profiler
--- sudoku_smt_solvers/solvers/__init__.py
+++ sudoku_smt_solvers/solvers/__init__.py
@@ -1,4 +1,3 @@
 from .cvc5_solver import CVC5Solver
 from .dpll_solver import DPLLSolver
-from .dpll_t_solver import DPLLTSolver
 from .z3_solver import Z3Solver
